42bafe391b83bca8bb726cbf6c4fa68d8290d8f6
Zach Rossen <zachary.rossen@gmail.com>
2022-09-14 09:17:08 -0700

    testing blob

--- src/BlobTest.java
+++ src/BlobTest.java
@@ -1,12 +1,27 @@
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.Test;
 
 public class BlobTest {
 
+	//creates file with name and contents
+	private void createFile(String name, String contents) throws IOException {
+		File file = new File(name);	
+		FileWriter fw = new FileWriter("objects/"+ file);
+		fw.write(contents);
+		fw.close();
+	}
+	
 	@Test
-	public void testBlob() {
-		fail("Not yet implemented");
+	public void testBlob() throws NoSuchAlgorithmException, IOException {
+		createFile("test.txt", "contents");
+		Blob test = new Blob("test.txt");
+		assertTrue(File.exists(test.getSha1()));
 	}
 
 	@Test
