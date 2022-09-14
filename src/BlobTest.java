f8de93b82efdcbe1ed62a7924c4d37cecb284fe1
Zach Rossen <zachary.rossen@gmail.com>
2022-09-14 09:01:03 -0700

    creating testers

---
+++ src/BlobTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BlobTest {
+
+	@Test
+	public void testBlob() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSha1() {
+		fail("Not yet implemented");
+	}
+}
