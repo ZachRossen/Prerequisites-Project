7b604f24446fec99b2f20adeb2802463d2c33e88
Zach Rossen <zachary.rossen@gmail.com>
2022-09-15 12:05:24 -0700

    creating correct tester classes

---
+++ src/BlobTester.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class BlobTester {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void PLEASEDDONTRUTHIS() {
+		
+	}
+
+}
